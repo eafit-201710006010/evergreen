789d0fc7de5ef9c44f6f574c86a321869ac0cfad
Evelezc1 <evelezc1@eafit.edu.co>
2019-10-23 14:25:13 -0500

    Add files via upload

---
+++ db.py
@@ -0,0 +1,8 @@
+import mysql.connector as mysql
+conn = mysql.MySQLConnection(
+    host="db-server-evergreen-estefania.mysql.database.azure.com",
+    port=3306,
+    user="evergreenadmin@db-server-evergreen-estefania",
+    password="xofJQP33",
+    database="evergreen"
+)

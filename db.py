a532df834ee7d4bf16ffc05c346016f4873c987f
Evelezc1 <evelezc1@eafit.edu.co>
2019-10-23 14:36:37 -0500

    Delete db.py

--- db.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector as mysql
-conn = mysql.MySQLConnection(
-    host="db-server-evergreen-estefania.mysql.database.azure.com",
-    port=3306,
-    user="evergreenadmin@db-server-evergreen-estefania",
-    password="xofJQP33",
-    database="evergreen"
-)
